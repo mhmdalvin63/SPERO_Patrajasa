cf328c6aa9e4be27e87cc0b649e54d04a48cf079
muhammadalvin92 <alvinbomber87@gmail.com>
2023-10-24 04:23:32 +0700

    Fitur Chat

--- src/Pages/PageDetailTicket.js
+++ src/Pages/PageDetailTicket.js
@@ -197,20 +197,15 @@ function DetailComponent() {
                                 {/* <hr /> */}
                                 <div className='element-chat'>
                                     <ol class="chat">
-                                        <li class="other">
+                                    {Chat.map((item, id) => (
+                                        <li key={id} className={item.user_id === 3 ? 'self' : 'other'}>
                                             <div className="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
                                         <div className="msg">
-                                            <h4>Lorem ipsum dolor sit amet consectetur. Quis interdum nulla nibh ut. Donec eu placerat at senectus. Molestie tristique ac sed velit eget a.</h4>
-                                            <time>20:17</time>
+                                            <h4>{item.content}</h4>
+                                            <time>{getTimeFromData(item.created_at)}</time>
                                         </div>
                                         </li>
-                                        <li class="self">
-                                            <div className="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
-                                        <div className="msg">
-                                            <h4>Lorem ipsum dolor sit amet consectetur. Quis interdum nulla nibh ut. Donec eu placerat at senectus. Molestie tristique ac sed velit eget a.</h4>
-                                            <time>20:18</time>
-                                        </div>
-                                        </li>
+                                    ))}
                                     </ol>
                                 </div>
                             </div>
