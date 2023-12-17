4d8a9028b91dbf9fbbfbc1b6b8d16e082006ed4f
muhammadalvin92 <alvinbomber87@gmail.com>
2023-12-17 16:36:02 +0700

    update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     if (currentURL.includes('172.16.16.3:3000')) {
       urlApi = process.env.REACT_APP_API_URL_HTTP;
     } else if (currentURL.includes('dashboard.par.co.id')) {
-      urlApi = process.env.REACT_APP_API_URL_HTTP;
+      urlApi = process.env.REACT_APP_API_URL;
     } else {
       urlApi = process.env.REACT_APP_API_URL;
     }
